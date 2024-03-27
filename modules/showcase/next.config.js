9696d53c0f040d5a77ccb7fdd78f4f2efb807562
Hasan Ayan <hasanayan@me.com>
2024-03-27 21:27:46 +0300

    env updates

--- modules/showcase/next.config.js
+++ modules/showcase/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.symlinks = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
